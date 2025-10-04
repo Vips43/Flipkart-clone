16247c104a29d9b264315c756dc9e01047797369
Vipul Gehlot <vipulgehlot43@gmail.com>
2025-10-04 17:41:17 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,194 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+
+    let Colgates = document.getElementById("Colgate")
+    let Mobiles = document.getElementById("Mobile")
+    let Footwear = document.getElementById("Footwear")
+    shoppingCart = document.querySelector('#shopping_cart')
+
+    let buttons = document.querySelectorAll('.btn')
+    let product = document.querySelectorAll('.product')
+
+    
+    function sectionNavigate() {
+        buttons.forEach(btn => {
+            const tabID = btn.dataset.set;
+            btn.addEventListener('click', () => {
+                buttons.forEach(b => b.classList.remove('active'))
+                btn.classList.add('active')
+
+                product.forEach(pr => {
+                    const prID = pr.dataset.set;
+                    pr.classList.remove('active')
+                    if (prID === tabID) {
+                        pr.classList.add('active')
+                    }
+
+                })
+            })
+        })
+    }
+
+    sectionNavigate()
+
+    colgateData.map(e => {
+        Colgates.innerHTML +=
+            `<div class="card p-2 bg-gray-100 flex flex-col justify-between items-center gap-2 shadow-md hover:scale-[0.99] transition-all rounded-lg ">
+            <div class="w-full border border-gray-200">
+                <img class="w-full" src=${e.img} alt="">
+            </div>
+            <div class="flex flex-col w-full">
+            <p class="grow">
+            <span class="font-semibold">
+                ${e.colName} 
+            </span> <br>
+            </p>
+            <div class="flex justify-between flex-wrap items-center">
+            <p class='flex gap-2'>
+                <span class="text-xs text-gray-400 line-through">
+                ${e.oldPrice}₹</span>
+            <span class="text-xs font-bold">${e.currPrice}₹</span>
+            <span class="text-sm text-green-500">${e.disc}%</span>
+            </p>
+            
+            <button
+            class="add-btn bg-blue-500 text-[0.6rem] h-6 py-1 px-2 self-end rounded-sm whitespace-nowrap hover:bg-blue-700 cursor-pointer" onclick='addToCart(this)'>Add to Cart</button>
+
+            <div class="qty-controls hidden justify-between items-stretch h-6 gap-1 rounded">
+                <div onclick="Dec(this)"
+                  class="px-2 py-1 font-bold flex items-center justify-center text-[0.6rem] border bg-blue-600 text-white cursor-pointer rounded-sm">
+                  -
+                </div>
+                <div class="px-1 flex items-center justify-center text-[0.6rem] rounded-sm">0</div>
+                <div onclick="Inc(this)"
+                  class="px-2 font-bold flex items-center justify-center text-[0.6rem] border bg-blue-600 text-white cursor-pointer rounded-sm">
+                  +
+                </div>
+              </div>
+
+
+            </div>
+            </div>
+        </div>`
+    })
+    mobileData.map(e => {
+        Mobiles.innerHTML +=
+            `<div class="card p-2 bg-gray-100 flex flex-col justify-between items-center gap-2 shadow-md hover:scale-[0.99] transition-all rounded-lg">
+            <div class="w-full border border-gray-200">
+                <img class="w-full" src=${e.img} alt="">
+            </div>
+            <div class="flex flex-col w-full">
+            <p class="grow">
+            <span class="font-semibold">
+                ${e.mobName} 
+            </span> <br>
+            </p>
+            <div class="flex justify-between flex-wrap items-center">
+            <p class='flexx gap-2'>
+                <span class="text-xs text-gray-400 line-through">
+                ${e.oldPrice}₹</span>
+            <span class="text-xs font-bold">${e.currPrice}₹</span>
+            <span class="text-sm text-green-500">${e.disc}%</span>
+            </p>
+        
+            <button
+            class="add-btn bg-blue-500 text-[0.6rem] h-6 py-1 px-2 self-end rounded-sm whitespace-nowrap hover:bg-blue-700 cursor-pointer" onclick='addToCart(this)'>Add to Cart</button>
+
+            <div class="qty-controls hidden justify-between items-stretch h-6 gap-1 rounded">
+                <div onclick="Dec(this)"
+                  class="px-2 py-1 font-bold flex items-center justify-center text-[0.6rem] border bg-blue-600 text-white cursor-pointer rounded-sm">
+                  -
+                </div>
+                <div class="px-1 flex items-center justify-center text-[0.6rem] rounded-sm">0</div>
+                <div onclick="Inc(this)"
+                  class="px-2 font-bold flex items-center justify-center text-[0.6rem] border bg-blue-600 text-white cursor-pointer rounded-sm">
+                  +
+                </div>
+              </div>
+    
+    
+            </div>
+            </div>
+        </div>`
+    })
+
+    footwearData.map(e => {
+        Footwear.innerHTML +=
+            `<div class="card p-2 bg-gray-100 flex flex-col justify-between items-center gap-2 shadow-md hover:scale-[0.99] transition-all rounded-lg">
+            <div class="w-full border border-gray-200">
+                <img class="w-full" src=${e.img} alt="">
+            </div>
+            <div class="flex flex-col w-full">
+            <p class="grow">
+            <span class="font-semibold">
+                ${e.mobName} 
+            </span> <br>
+            </p>
+            <div class="flex justify-between flex-wrap items-center">
+            <p class='flex gap-2'>
+                <span class="text-xs text-gray-400 line-through">
+                ${e.oldPrice}₹</span>
+            <span class="text-xs font-bold">${e.currPrice}₹</span>
+            <span class="text-xs text-green-500">${e.disc}%</span>
+            </p>
+        
+            <button
+            class="add-btn bg-blue-500 text-[0.6rem] h-6 py-1 px-2 self-end rounded-sm whitespace-nowrap hover:bg-blue-700 cursor-pointer" onclick='addToCart(this)'>Add to Cart</button>
+
+            <div class="qty-controls hidden justify-between items-stretch h-6 gap-1 rounded">
+                <div onclick="Dec(this)"
+                  class="px-2 py-1 font-bold flex items-center justify-center text-[0.6rem] border bg-blue-600 text-white cursor-pointer rounded-sm">
+                  -
+                </div>
+                <div class="px-1 flex items-center justify-center text-[0.6rem] rounded-sm">0</div>
+                <div onclick="Inc(this)"
+                  class="px-2 font-bold flex items-center justify-center text-[0.6rem] border bg-blue-600 text-white cursor-pointer rounded-sm">
+                  +
+                </div>
+              </div>
+    
+            </div>
+            </div>
+        </div>`
+    })
+})
+
+
+function addToCart(button) {
+    const card = button.closest('.card')
+    const qtyControls = card.querySelector('.qty-controls')
+    let counter = qtyControls.querySelector('div:nth-child(2)')
+
+    button.classList.add('hidden')
+    qtyControls.classList.replace('hidden', 'flex')
+
+    counter.textContent = 1
+    console.log(counter.textContent);
+}
+function Dec(btn){
+    const card = btn.closest('.card')
+    const counter = card.querySelector('.qty-controls div:nth-child(2)')
+    let current = parseInt(counter.textContent)
+    if(current > 0){
+        current--
+        counter.textContent = current
+    }
+    if(counter.textContent == 0){
+        card.querySelector('.add-btn').classList.remove('hidden')
+        card.querySelector('.qty-controls').classList.add('hidden')
+    }
+}
+function Inc(btn){
+    const card = btn.closest('.card')
+    const counter = card.querySelector('.qty-controls div:nth-child(2)')
+    let current = parseInt(counter.textContent);
+    current++;
+    console.log(current);
+    
+    counter.textContent = current;
+}
+
+
+
+
 
